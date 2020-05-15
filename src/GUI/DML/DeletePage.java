f60c07b6b1479d49d08a917972ec3bab7d47bf1e
snikhil1998 <35304699+snikhil1998@users.noreply.github.com>
2020-05-15 11:10:48 +0530

    Update DeletePage.java

--- src/GUI/DML/DeletePage.java
+++ src/GUI/DML/DeletePage.java
@@ -23,7 +23,7 @@ class DeletePage {
 	public DeletePage() {
 		frame = new JFrame("Delete Operation");
 		frame.setSize(800,600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		controlPanel = new JPanel();
 		controlPanel.setLayout(null);

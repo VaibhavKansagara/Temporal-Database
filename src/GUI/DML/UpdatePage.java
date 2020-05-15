c04daf5479ae24de73ea1d3e406c4c601159109f
snikhil1998 <35304699+snikhil1998@users.noreply.github.com>
2020-05-15 11:11:21 +0530

    Update UpdatePage.java

--- src/GUI/DML/UpdatePage.java
+++ src/GUI/DML/UpdatePage.java
@@ -23,7 +23,7 @@ class UpdatePage {
 	public UpdatePage() {
 		frame = new JFrame("Update Operation");
 		frame.setSize(800,600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		controlPanel = new JPanel();
 		controlPanel.setLayout(null);
